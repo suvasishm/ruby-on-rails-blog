75de0e23c8fbd25e4639c2f834e957023547dc9d
Suvasish Mondal <suvasish.mondal@datarobot.com>
2020-05-22 15:41:45 -0500

    Initial commit

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "suv", password: "pass", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
